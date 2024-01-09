3364d18062a491911a7d0cfef9c517b0fbd63276
manoj <gandhamanojkutty@gmail.com>
2024-01-09 11:09:35 +0530

    code updated

---
+++ env-prod/main.tfvars
@@ -0,0 +1,44 @@
+components = {
+  frontend = {
+    name = "frontend"
+    instance_type = "t3.micro"
+  }
+  mongodb = {
+    name = "mongodb"
+    instance_type = "t3.micro"
+  }
+  catalogue = {
+    name = "catalogue"
+    instance_type = "t3.small"
+  }
+  redis = {
+    name = "redis"
+    instance_type = "t3.micro"
+  }
+  user = {
+    name = "user"
+    instance_type = "t3.micro"
+  }
+  cart = {
+    name = "cart"
+    instance_type = "t3.small"
+  }
+  mysql = {
+    name = "mysql"
+    instance_type = "t3.micro"
+  }
+  shipping = {
+    name = "shipping"
+    instance_type = "t3.medium"
+  }
+  rabbitmq = {
+    name = "rabbitmq"
+    instance_type = "t3.micro"
+  }
+  payment = {
+    name = "payment"
+    instance_type = "t3.medium"
+  }
+}
+
+env = "prod"
--- servers.tf
+++ servers.tf
@@ -1,58 +1,3 @@
-data "aws_ami" "centos" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-}
-
-data "aws_security_group" "allow-all" {
-  name = "allow all"
-}
-
-variable "components" {
-  default = {
-    frontend = {
-      name = "frontend"
-      instance_type = "t3.micro"
-    }
-    mongodb = {
-      name = "mongodb"
-      instance_type = "t3.micro"
-    }
-    catalogue = {
-      name = "catalogue"
-      instance_type = "t3.small"
-    }
-    redis = {
-      name = "redis"
-      instance_type = "t3.micro"
-    }
-    user = {
-      name = "user"
-      instance_type = "t3.micro"
-    }
-    cart = {
-      name = "cart"
-      instance_type = "t3.small"
-    }
-    mysql = {
-      name = "mysql"
-      instance_type = "t3.micro"
-    }
-    shipping = {
-      name = "shipping"
-      instance_type = "t3.medium"
-    }
-    rabbitmq = {
-      name = "rabbitmq"
-      instance_type = "t3.micro"
-    }
-    payment = {
-      name = "payment"
-      instance_type = "t3.medium"
-    }
-  }
-}
-
 resource "aws_instance" "instance" {
   for_each = var.components
   ami           = data.aws_ami.centos.image_id
@@ -63,8 +8,6 @@ resource "aws_instance" "instance" {
     Name = each.value["name"]
   }
 }
-
-
 resource "aws_route53_record" "frontend" {
   for_each = var.components
   zone_id = "Z07849831FFYEHNKDVTOV"
