4d6e03883593b228a9e428b4cacdd305a2c3a979
manoj <gandhamanojkutty@gmail.com>
2024-01-23 11:05:46 +0530

    code updated

--- date.tf
+++ date.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "centos" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
+  owners           = ["851725215534"]
 }
 
 data "aws_security_group" "allow-all" {
