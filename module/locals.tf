efab4634748b58650dca4eae5fd018e06ec9c420
manoj <gandhamanojkutty@gmail.com>
2024-02-07 12:03:02 +0530

    code updated

--- module/locals.tf
+++ module/locals.tf
@@ -2,12 +2,12 @@ locals {
   name = var.env != "" ? "${var.component_name}-${var.env}" : var.component_name
   db_commands = [
     "rm -rf roboshop-shell",
-    "git clone https://github.com/manoj-gandham/roboshop-shell.git",
+    "git clone https://github.com/manoj-gandham/roboshop-shell",
     "cd roboshop-shell",
     "sudo bash ${var.component_name}.sh ${var.password}"
   ]
   app_commands = [
-  "sudo labauto ansible",
-    "ansible-playbook -i localhost, -U https://github.com/manoj-gandham/roboshop-ansible.git roboshop.yml -e env=${var.env} -e role_name= ${var.component_name}"
+    "sudo labauto ansible",
+    "ansible-pull -i localhost, -U https://github.com/manoj-gandham/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
 }
