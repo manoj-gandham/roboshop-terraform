60a02b61e8b34608e94fd433264d57d8a4ac5e2f
manoj <gandhamanojkutty@gmail.com>
2024-02-27 18:54:48 +0530

    code updated

--- module/locals.tf
+++ module/locals.tf
@@ -10,4 +10,11 @@ locals {
     "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/manoj-gandham/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
+  db_tags = {
+    Name = "${var.component_name}-${var.env}"
+  }
+  app_tags = {
+    Name    = "${var.component_name}-${var.env}"
+    Monitor = "true"
+  }
 }
