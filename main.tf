1a9933ce4cfd8a5e17c27066a18b8d73933dfc9e
manoj <gandhamanojkutty@gmail.com>
2024-03-17 13:46:31 +0530

    code updated

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 module "vpc" {
   source = "git::https://github.com/manoj-gandham/tf-module-vpc.git"
-  for_each = var.vpc
+
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
-  subnets = each.value["subnets"]
-  tags = local.tags
-  env = var.env
+  subnets    = each.value["subnets"]
+  tags       = local.tags
+  env        = var.env
 }
+
 
 module "web" {
   source = "git::https://github.com/manoj-gandham/tf-module-app.git"
