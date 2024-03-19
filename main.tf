0e5557dfaa48149a9b768f44c6b48b4fa70d45a3
manoj <gandhamanojkutty@gmail.com>
2024-03-19 22:24:34 +0530

    code updated

--- main.tf
+++ main.tf
@@ -9,10 +9,20 @@ module "vpc" {
 }
 
 
-module "web" {
+module "app" {
   source = "git::https://github.com/manoj-gandham/tf-module-app.git"
 
   for_each      = var.app
   instance_type = each.value["instance_type"]
-  subnet_id     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+  name = each.value["name"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size = ["max_size"]
+  min_size = ["min_size"]
+
+  env = var.env
+  bastion_cidr = var.bastion_cidr
+
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
 }
