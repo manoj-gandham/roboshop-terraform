4f4af15034f9ce44e64d0f8fec390b6027f63b94
manoj <gandhamanojkutty@gmail.com>
2024-03-22 15:46:09 +0530

    code updated

--- main.tf
+++ main.tf
@@ -119,6 +119,6 @@ module "app" {
   vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
   listener_arn = lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null)
-  lb_dns_name = lookup(lookup(module.alb, each.value["lb_type"], null), "lb_dns_name", null)
+  lb_dns_name = lookup(lookup(module.alb, each.value["lb_type"], null), "dns_name", null)
 }
 
